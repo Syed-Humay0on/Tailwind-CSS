92625bb7bc685b6e5d49ec264e7a4975b60ce966
Proto <humayoon04syed@gmail.com>
2024-10-08 01:31:33 +0500

    Tailwind-flowbite {Dark theme added (body theme worked)}

--- src/script.js
+++ src/script.js
@@ -4,11 +4,13 @@ const bodyElement = document.body;
 // Load theme from localStorage
 if (localStorage.getItem('theme') === 'dark') {
   bodyElement.classList.add('dark');
+  bodyElement.style.backgroundColor = 'rgb(17, 24, 39)';
 }
 
 // Toggle dark class and save preference
 themeToggleBtn.addEventListener('click', () => {
   bodyElement.classList.toggle('dark');
+
 
   if (bodyElement.classList.contains('dark')) {
     localStorage.setItem('theme', 'dark');
